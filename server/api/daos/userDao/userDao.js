4d044dbe1c9d4453edddac78847bb80b9d22ae66
Abdeljalil NACEUR <contact@naceur-abdeljalil.com>
2020-07-02 00:56:04 +0200

    SERVER/API : - Send activation link added.

--- server/api/daos/userDao/userDao.js
+++ server/api/daos/userDao/userDao.js
@@ -3,6 +3,7 @@ const UserClass = require("../../class/UserClass");
 const utils = require('../../utils/utils');
 const userEmailsTemplate = require('../../emails/userEmails/accountValidationEmail');
 const emailSender = require('../../utils/emailSender');
+const AccessToken = require('../../class/accessTokenClass');
 
 ifExistUserAccount = (email) => {
     return new Promise((resolve, reject) => {
@@ -29,9 +30,11 @@ ifExistUserAccountById = (id) => {
 
 saveNewUserAccount = (data) => {
     return new Promise(async (resolve, reject) => {
-        let user = new User(await UserClass.CreateNewUser(data));
+        const accessTokenDao = new AccessToken();
+        const token = await accessTokenDao.generateToken(data.email, data.username);
+        let user = new User(await UserClass.CreateNewUser(data, token));
         user.save().then(res => {
-            let msg = userEmailsTemplate.accountValidation();
+            let msg = userEmailsTemplate.accountValidation(token);
             emailSender.sendEmail("MUSICROOM TEAM", data.email, "Account validation", msg);
             resolve(true);
         }).catch(err => {
--- server/api/emails/userEmails/accountValidationEmail.js
+++ server/api/emails/userEmails/accountValidationEmail.js
@@ -1,8 +1,8 @@
 
-const accountValidation = () => {
+const accountValidation = (token) => {
     let msg = `Hello ! \n  \
     \n Here is your activation link : \
-    \n  ${process.env.URL_BACKEND + ":" + process.env.URL_BACKEND_PORT} \
+    \n  ${process.env.URL_BACKEND + ":" + process.env.URL_BACKEND_PORT + "/api/v1/users/validation/" + token} \
     \n MUSICROOM TEAM`
 
     return msg
