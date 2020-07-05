884d2acd0da05e247c92d937057cec1ccc51f10d
Abdeljalil NACEUR <contact@naceur-abdeljalil.com>
2020-07-05 11:58:44 +0200

    OAUTH@

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -5,6 +5,7 @@ const VERSION_API = process.env.REACT_APP_API_VERSION;
 
 // TODO CHECK WHY ENV NOT DETECTING
 export default Axios.create({
-    baseURL: "http://192.168.42.120:3000/api/v1/"
+    // baseURL: "http://192.168.42.120:3000/api/v1/"
+    baseURL: "https://42music.pagekite.me/api/v1/"
 })
 
