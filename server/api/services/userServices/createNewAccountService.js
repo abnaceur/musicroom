89375be671ce98d576333e43e7722bf7d415f3b9
Dove Pa <dove@frenchgorilla.com>
2020-07-06 23:55:45 +0200

    add regex.

--- server/api/services/userServices/createNewAccountService.js
+++ server/api/services/userServices/createNewAccountService.js
@@ -1,9 +1,34 @@
 const userDao = require('../../daos/userDao/userDao');
+const utils = require('../../utils/utils');
 
 async function addNewUser(req, data, res) {
     //check password length
     // TODO ADD INPUT VALIDATION
-    if (data.password && data.password.length > 8) {
+    if (!data || !data.email || utils.mailRegex.test(data.email) === false) {
+        res.status(200).json({
+            success: false,
+            data: {
+                msg: "Regex error mail"
+            },
+            code: 407
+        })
+    } else if (!data || !data.username || utils.userNameRegex.test(data.username) === false) {
+        res.status(200).json({
+            success: false,
+            data: {
+                msg: "Regex error username"
+            },
+            code: 408
+        })
+    } else if (!data || !data.password || utils.passwordRegex.test(data.password) === false) {
+        res.status(200).json({
+            success: false,
+            data: {
+                msg: "Regex error password"
+            },
+            code: 409
+        })
+    } else if (data.password && data.password.length > 8) {
         // check if email exist
         if (await userDao.ifExistUserAccount(data.email) === 0) {
             if (await userDao.saveNewUserAccount(data)) {
@@ -14,7 +39,7 @@ async function addNewUser(req, data, res) {
                     },
                     code: 200
                 })
-            } else { 
+            } else {
                 res.status(500);
             }
         } else {
