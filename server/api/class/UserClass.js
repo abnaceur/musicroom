cebf4c930c3f274ab3ac25bbd9cba38036d16927
Abdeljalil NACEUR <contact@naceur-abdeljalil.com>
2020-07-07 14:22:48 +0200

    OAUTH42 : - Added.

--- server/api/class/UserClass.js
+++ server/api/class/UserClass.js
@@ -31,7 +31,22 @@ async function creatNewOauth2User(data) {
 
 }
 
+async function creatNewOauth2UserVia42 (userInfo) {
+    return new Promise(async (resolve, reject) => {
+        resolve({
+            _id: new mongoose.Types.ObjectId,
+            googleId: userInfo.id,
+            googlePhoto: userInfo.photos[0].value,
+            email: userInfo.emails[0].value,
+            username: userInfo.displayName,
+            validationToken: userInfo.id,
+            active: true,
+        })
+    });
+}
+
 module.exports = {
     CreateNewUser,
+    creatNewOauth2UserVia42,
     creatNewOauth2User
 }
--- server/api/services/userServices/userOauth2GoogleService.js
+++ server/api/services/userServices/userOauth2GoogleService.js
@@ -29,8 +29,7 @@ async function loginOauth2User(res, userInfo) {
                 const token = await accessTokenDao.generateToken(userInfo.id !== undefined ? userInfo.id : usrOne[0]._id, usrOne[0]._id);
                 const accessTokenValue = await accessTokenDao.saveTokenAndGetAccessToken(token, usrOne[0]._id);
                 const basedAccesstoken = await accessTokenDao.generateToken(accessTokenValue, usrOne[0]._id);
-    
-                console.log('===========================AAA==')
+
                 res.redirect("msrm42app://msrm42app.io?token=" + basedAccesstoken +
                     "&userId=" + usrOne[0]._id + "&givenName=" +
                     usrOne[0].username);
@@ -39,6 +38,43 @@ async function loginOauth2User(res, userInfo) {
         .catch(err => utils.defaultError(res, err))
 }
 
+async function loginOauth2UserVia42(res, userInfo) {
+    const accessTokenDao = new AccessToken();
+
+    User.find({
+        "email": userInfo.emails[0].value
+    })
+        .exec()
+        .then(async usrOne => {
+            if (usrOne.length === 0) {
+                let user = new User(await userClass.creatNewOauth2UserVia42(userInfo));
+                user.save()
+                    .then(async usr => {
+                        const token = await accessTokenDao.generateToken(userInfo.id !== undefined ? userInfo.id : usr._id, usr._id);
+                        const accessTokenValue = await accessTokenDao.saveTokenAndGetAccessToken(token, usr._id);
+                        const basedAccesstoken = await accessTokenDao.generateToken(accessTokenValue, usr._id);
+
+                        res.redirect("msrm42app://msrm42app.io?token=" + basedAccesstoken +
+                            "&userId=" + usr._id + "&givenName=" +
+                            usr.username);
+                    })
+                    .catch(err => utils.defaultError(res, err));
+            } else {
+
+                const token = await accessTokenDao.generateToken(userInfo.googleId !== undefined ? userInfo.googleId : usrOne[0]._id, usrOne[0]._id);
+                const accessTokenValue = await accessTokenDao.saveTokenAndGetAccessToken(token, usrOne[0]._id);
+                const basedAccesstoken = await accessTokenDao.generateToken(accessTokenValue, usrOne[0]._id);
+
+                res.redirect("msrm42app://msrm42app.io?token=" + basedAccesstoken +
+                    "&userId=" + usrOne[0]._id + "&givenName=" +
+                    usrOne[0].username);
+            }
+        })
+        .catch(err => utils.defaultError(res, err))
+}
+
+
 module.exports = {
-    loginOauth2User
+    loginOauth2User,
+    loginOauth2UserVia42
 }
