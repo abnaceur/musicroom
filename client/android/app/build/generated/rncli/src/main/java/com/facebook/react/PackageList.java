03c97d8923d7dc8ce83d91a5193560d4ba343f2a
Abdeljalil NACEUR <contact@naceur-abdeljalil.com>
2020-07-13 14:23:52 +0200

    CLIENT : - Add playlist details.

--- client/android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ client/android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -15,11 +15,8 @@ import java.util.ArrayList;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 // @react-native-community/checkbox
 import com.reactnativecommunity.checkbox.ReactCheckBoxPackage;
-<<<<<<< HEAD
 // @react-native-community/datetimepicker
 import com.reactcommunity.rndatetimepicker.RNDateTimePickerPackage;
-=======
->>>>>>> def2069e6519b3963d0d9bd2f587f3990c79bce1
 // @react-native-community/masked-view
 import org.reactnative.maskedview.RNCMaskedViewPackage;
 // @react-native-community/slider
@@ -83,10 +80,7 @@ public class PackageList {
       new MainReactPackage(mConfig),
       new AsyncStoragePackage(),
       new ReactCheckBoxPackage(),
-<<<<<<< HEAD
       new RNDateTimePickerPackage(),
-=======
->>>>>>> def2069e6519b3963d0d9bd2f587f3990c79bce1
       new RNCMaskedViewPackage(),
       new ReactSliderPackage(),
       new RNGestureHandlerPackage(),
---
+++ client/src/screens/PlayListDetails.js
@@ -0,0 +1,113 @@
+import React, { useContext, useEffect, useState } from 'react';
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    ScrollView,
+    Dimensions,
+    Image,
+    KeyboardAvoidingView,
+    StyleSheet
+} from 'react-native';
+
+// Import context
+import { Context as AuthContext } from '../context/AuthContext';
+import { Card, Tile, ListItem, Button, Header, Icon } from "react-native-elements";
+import FavOff from "react-native-vector-icons/MaterialIcons";
+import BackWard from "react-native-vector-icons/Ionicons";
+
+const PlaylistDetailsScreens = (props) => {
+    const { signout } = useContext(AuthContext);
+    const [listDetails, setDetails] = useState({});
+    const { navigation, route } = props;
+
+
+    useEffect(() => {
+        if (route.params?.playListDetails) {
+            let test = JSON.parse(route.params.playListDetails);
+            console.log("route.params :", test);
+            setDetails(JSON.parse(route.params.playListDetails))
+        }
+    }, [])
+    // React.useEffect(() => {
+    //     if (route.params?.post) {
+    //       // Post updated, do something with `route.params.post`
+    //       // For example, send the post to the server
+    //     }
+    //   }, [route.params?.post]);
+
+    return (
+        <ScrollView style={Styles.container}>
+            <View>
+                <Header
+                    backgroundColor="#633689"
+                    centerComponent={{ text: listDetails.name, style: { color: "#fff" } }}
+                    leftComponent={
+                        <BackWard
+                            onPress={() => navigation.goBack()}
+                            name="md-arrow-back"
+                            size={24}
+                            color="white"
+                        />
+                    }
+                    rightComponent={
+                        // name favoris for desactivate
+                        <FavOff name="favorite-border" size={24} color="white" />
+                    }
+                />
+
+                <Tile
+                    imageSrc={require('../assets/music.jpg')}
+                    title={listDetails.name}
+                    featured
+                    caption={listDetails.desctiption}
+                />
+
+                {
+                    listDetails.trackList ? listDetails.trackList.map((l, i) => (
+                        <ListItem
+                            key={i}
+                            leftAvatar={{ source: { uri: l.album.cover_big } }}
+                            title={l.title}
+                            // subtitle={l.subtitle}
+                            bottomDivider
+                        />
+                    )) : <Text>Empty list</Text>
+                }
+
+            </View>
+        </ScrollView>
+    )
+}
+
+const Styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#282830',
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+    logoContainer: {
+        color: 'white',
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    logo: {
+        width: 100,
+        height: 100
+    },
+    title: {
+        color: '#fff',
+        marginTop: 20,
+        fontWeight: "100",
+        fontSize: 23
+    },
+    myForm: {
+        flex: 3
+    },
+
+})
+
+
+export default PlaylistDetailsScreens;
