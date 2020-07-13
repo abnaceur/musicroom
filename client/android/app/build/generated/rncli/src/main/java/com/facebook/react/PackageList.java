543e912f5365f15ac000cfa303a9c90fb2ccf46f
syboeuf <syboeuf@student.42.fr>
2020-07-13 10:51:28 +0200

    Add page create event

--- client/android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ client/android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -13,6 +13,10 @@ import java.util.ArrayList;
 
 // @react-native-community/async-storage
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+// @react-native-community/checkbox
+import com.reactnativecommunity.checkbox.ReactCheckBoxPackage;
+// @react-native-community/datetimepicker
+import com.reactcommunity.rndatetimepicker.RNDateTimePickerPackage;
 // @react-native-community/masked-view
 import org.reactnative.maskedview.RNCMaskedViewPackage;
 // @react-native-community/slider
@@ -75,6 +79,8 @@ public class PackageList {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
       new MainReactPackage(mConfig),
       new AsyncStoragePackage(),
+      new ReactCheckBoxPackage(),
+      new RNDateTimePickerPackage(),
       new RNCMaskedViewPackage(),
       new ReactSliderPackage(),
       new RNGestureHandlerPackage(),
