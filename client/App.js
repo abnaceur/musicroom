9384e3a42d889c84c68091a730901340e92c032a
Abdeljalil NACEUR <contact@naceur-abdeljalil.com>
2020-07-20 00:56:03 +0200

    CLIENT/SERVER : - Call apo

--- client/App.js
+++ client/App.js
@@ -98,8 +98,8 @@ function TabStack() {
 
 const App = () => {
   const { state } = useContext(AuthContext);
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IiQyYiQxMCQySW84ckxhRGNMTC9XVE45Q3N6MzQubk4zNWNrZUxpc3hzaW9ZV0FUclAvN3JkemZGb3FMTyIsImRhdGEiOiI1ZjBkOGQ2Mzc3NjMwNjAwOGFmZGY4MGUiLCJpYXQiOjE1OTQ5OTMzMTMsImV4cCI6MTU5NTAzNjUxM30.FVGmZOBa9BAlt4hMAe3BTExWYiiG-7plHpZv1mRcX9k";
-  // const token = state.token;
+  // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IiQyYiQxMCQySW84ckxhRGNMTC9XVE45Q3N6MzQubk4zNWNrZUxpc3hzaW9ZV0FUclAvN3JkemZGb3FMTyIsImRhdGEiOiI1ZjBkOGQ2Mzc3NjMwNjAwOGFmZGY4MGUiLCJpYXQiOjE1OTQ5OTMzMTMsImV4cCI6MTU5NTAzNjUxM30.FVGmZOBa9BAlt4hMAe3BTExWYiiG-7plHpZv1mRcX9k";
+  const token = state.token;
   
   if (!token) {
     return (
